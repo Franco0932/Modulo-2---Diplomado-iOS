58c910d05826352609ec2aa34c50faabcaaf5f21
Franco Ruiz <francoruiz287@gmail.com>
2025-10-03 17:10:11 -0600

    Initial Commit

---
+++ Ejer1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejer1
+//
+//  Created by Franco Ruiz on 03/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
